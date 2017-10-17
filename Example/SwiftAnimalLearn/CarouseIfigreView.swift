bbd013ca7e956fa719d279d008aed37a44af5bb4
shanli <shanli@2dfire.com>
2017-10-17 17:11:40 +0800

    提交一波二

---
+++ Example/SwiftAnimalLearn/CarouseIfigreView.swift
@@ -0,0 +1,21 @@
+//
+//  CarouseIfigreView.swift
+//  SwiftAnimalLearn_Example
+//
+//  Created by 山梨 on 2017/10/17.
+//  Copyright © 2017年 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class CarouseIfigreView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
